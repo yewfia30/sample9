15a9ef33a1b0e1f2466860a18e157198ad2ffc75
yewfia30 <yewfia30@gmail.com>
2016-03-26 22:06:41 +0900

    Initial Commit

---
+++ sample9/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample9
+//
+//  Created by yoshinobu on 2016/03/26.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample9/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sample9
+//
+//  Created by yoshinobu on 2016/03/26.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
